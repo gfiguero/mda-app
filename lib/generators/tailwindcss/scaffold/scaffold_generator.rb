307ea36b1197d445eb103f17fb357dcd7ffa8c0a
Garri Figueroa <gfiguero@gmail.com>
2025-08-15 13:58:54 -0400

    Convert ERB scaffold generator to extend TailwindCSS scaffold generator.

--- lib/generators/tailwindcss/scaffold/scaffold_generator.rb
+++ lib/generators/tailwindcss/scaffold/scaffold_generator.rb
@@ -1,20 +1,15 @@
-# frozen_string_literal: true
-
-require "rails/generators/erb"
+require "rails/generators/erb/scaffold/scaffold_generator"
 require "rails/generators/resource_helpers"
 
-module Erb
+module Tailwindcss
   module Generators
-    class ScaffoldGenerator < Base
+    class ScaffoldGenerator < Erb::Generators::ScaffoldGenerator
       include Rails::Generators::ResourceHelpers
 
-      # source_root File.expand_path("../templates", __FILE__)
+      source_root File.expand_path("templates", __dir__)
+      source_paths << "lib/templates/erb/scaffold"
 
       argument :attributes, type: :array, default: [], banner: "field:type field:type"
-
-      def create_root_folder
-        empty_directory File.join("app/views", controller_file_path)
-      end
 
       def copy_view_files
         available_views.each do |view|
